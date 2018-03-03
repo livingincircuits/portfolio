47bf3b95d23e3d2e7341f5cfd4374a30febd49ff
Emily Young <emily@emily-young.com>
2018-03-03 17:06:34 +0000

    Word Updates :sunglasses:

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -17,81 +17,34 @@ const About = () => (
       <h1>Hello World.</h1>
 
       <p>
-        I'm Emily and I am a UX designer.
+        I'm Emily and I am a UX designer and multipotentialite.
       </p>
 
       <h2>
         Knowledge & Expertise
       </h2>
 
+      <p>
+        I have a range of skills across the digital design and development world but the below are a few:
+      </p>
+
       <ul>
         <li>
-          UCD process
-        </li>
-        <li>
-          Working in an Agile team
-        </li>
-        <li>
-          Front End Development
+          Interaction &amp; UI Design
         </li>
         <li>
           User Research
         </li>
         <li>
-          Interaction &amp; UI Design
+          Front End Development
         </li>
         <li>
-          Prototyping &amp; Testing Including Hi-Fi &amp; Lo-Fi Wireframes
+          Prototyping
         </li>
         <li>
-          User Story Development
-        </li>
-        <li>
-          User Journey Mapping
-        </li>
-        <li>
-          Quantitative Survey
-        </li>
-        <li>
-          Competitive Audits
-        </li>
-        <li>
-          Persona Creation
-        </li>
-        <li>
-          A/B Testing
-        </li>
-        <li>
-          Conducting Usability Tests & Observations.
+          Conducting Usability Tests & Observations
         </li>
       </ul>
-
-      <h2>
-        Favourite Tools
-      </h2>
-
-      <ul>
-        <li>
-          Sketch
-        </li>
-        <li>
-          Invision
-        </li>
-        <li>
-          Marvel App
-        </li>
-        <li>
-          Zeplin
-        </li>
-      </ul>
-
-      <h2>
-        Study
-      </h2>
-
-      <p>
-        I hold an <a href="https://www.studydigital.co.uk/">FdA in Web Design</a>, enrolled at Interaction Design Foundation and currently studying Computing & IT and Psychology.
-      </p>
 
       <h2>
         Working
