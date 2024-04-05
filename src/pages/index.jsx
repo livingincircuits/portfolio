b6e6e9ce8a9ed45e3844ec2dd2992e4f5ea64dfd
Emily Young <ejyoung@protonmail.com>
2024-04-05 22:58:56 +0100

    Design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ const Index = ({
         </p>
         <p>
           With over a decade of experience, I have been instrumental in integrating UX and User Research methodologies into various teams. 
-          My expertise encompasses working with a wide range of stakeholders, including C-suite executives, Product Managers, Designers, Engineers, and Marketers, ultimately influencing decision-making processes and product strategies. 
+          My expertise encompasses working with a wide range of stakeholders and influencing decision-making processes and product strategies. 
           My approach is deeply rooted in a commitment to understanding user needs and behaviors, ensuring that every project I undertake is user-centered and designed for impact.
         </p>
       </div>
